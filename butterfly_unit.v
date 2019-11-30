f12a708e6403dbed9d14fd99dd9d95cb4589691d
Hersh Joshi <hersh.joshi1@gmail.com>
2019-11-30 15:53:16 -0800

    added ready flag to indicate computation finished

--- butterfly_unit.v
+++ butterfly_unit.v
@@ -9,10 +9,12 @@ module butterfly_unit(
 	input wire [data_size:0] i_data_rb,
 	input wire [data_size:0] i_data_cb,
 	input wire [3:0] twiddle_num,
+	input wire new_input_flag, // used for synchronization
 	output reg [data_size:0] o_data_ra,
 	output reg [data_size:0] o_data_ca,
 	output reg [data_size:0] o_data_rb,
-	output reg [data_size:0] o_data_cb
+	output reg [data_size:0] o_data_cb,
+	output reg ready_flag
     );
 	 
 	/*********************************************************************
@@ -49,9 +51,40 @@ module butterfly_unit(
 	assign i_twiddle_c_extend = {{(data_size+1){twiddle_imag[data_size]}}, twiddle_imag};
 	
 	/*********************************************************************
+	 * Sychronize All Outputs
+	 * 	Change ready_flag to high after a new input has been detected and 
+	 * 	4 clock cycles have been completed. 
+	 *		This indicates a high
+	 *********************************************************************/
+	reg [1:0] ready_flag_counter;
+	reg new_input_flag_prev_val;
+	
+	always @ (posedge clk) begin
+		//once the input is changed, count for 4 clocks cycles
+		if (rst) begin
+			ready_flag <= 0;
+			new_input_flag_prev_val <= 0;
+			ready_flag_counter <= 2'b0;
+		end															//how do i reset the ready_flag?
+		else if (new_input_flag^new_input_flag_prev_val) begin 	//new input received
+			if (ready_flag_counter == 3) begin							//new input has been reveived for 4 counts
+				ready_flag <= 1;												//indicate that new input has been processed
+				new_input_flag_prev_val <= new_input_flag;			//store new_flag value so counter ceases
+				ready_flag_counter <= 0;
+			end
+			else begin															//new input has been received but not for long enough
+				ready_flag_counter <= ready_flag_counter + 1;
+				ready_flag <= 0;
+			end
+		end
+	end
+	//reset ready flag when next new_input_flag is read
+	//toggle new_input_flag everytime a new input is recieved, starts 0->1
+	
+	/*********************************************************************
 	 * Butterfly Computation
 	 *********************************************************************/
-		 
+	
 	always @ (posedge clk, posedge rst)
 		if(rst) begin
 			o_data_ra <= 0;
@@ -65,13 +98,13 @@ module butterfly_unit(
 		end
 		else begin
 			// Multiplication
-			temp_r <= i_data_rb_extend*i_twiddle_r_extend + (~(i_data_cb_extend*i_twiddle_c_extend) + 1);
+			temp_r <= i_data_rb_extend*i_twiddle_r_extend + (~(i_data_cb_extend*i_twiddle_c_extend) + 1); //takes longer than temp_c, synchronize somehow
 			temp_c <= i_data_rb_extend*i_twiddle_c_extend + i_data_cb_extend*i_twiddle_r_extend;
 			mult_out_r <= temp_r[31:0];
 			mult_out_c <= temp_c[31:0];
 
 			// Addition
-			o_data_ra <= i_data_ra + mult_out_r[2*data_size:data_size];
+			o_data_ra <= i_data_ra + mult_out_r[2*data_size:data_size]; //& {16{1 || (temp_r && temp_c)}} -> ready flag
 			o_data_ca <= i_data_ca + mult_out_c[2*data_size:data_size];
 			o_data_rb <= i_data_ra + (~mult_out_r[2*data_size:data_size]+1);
 			o_data_cb <= i_data_ca + (~mult_out_c[2*data_size:data_size]+1);
--- butterfly_unit_tb.v
+++ butterfly_unit_tb.v
@@ -9,10 +9,12 @@ module butterfly_unit_tb;
 	reg [data_size:0] i_data_rb;
 	reg [data_size:0] i_data_cb;
 	reg [3:0] twiddle_num;
+	reg new_input_flag;
 	wire [data_size:0] o_data_ra;
 	wire [data_size:0] o_data_ca;
 	wire [data_size:0] o_data_rb;
 	wire [data_size:0] o_data_cb;
+	wire ready_flag;
 	
 	butterfly_unit but(	.clk(clk),
 								.rst(rst),
@@ -21,10 +23,12 @@ module butterfly_unit_tb;
 								.i_data_rb(i_data_rb),
 								.i_data_cb(i_data_cb),
 								.twiddle_num(twiddle_num),
+								.new_input_flag(new_input_flag),
 								.o_data_ra(o_data_ra),
 								.o_data_ca(o_data_ca),
 								.o_data_rb(o_data_rb),
-								.o_data_cb(o_data_cb));
+								.o_data_cb(o_data_cb),
+								.ready_flag(ready_flag));
 								
 	initial begin
 		clk = 0;
@@ -32,12 +36,14 @@ module butterfly_unit_tb;
 		#5 
 		rst = 0;
 		twiddle_num = 0;
+		new_input_flag = 0;
 		
 		#20
 		i_data_ra = 16'd2;
 		i_data_ca = 16'd4;
 		i_data_rb = 16'b1111111111111011; //-5
 		i_data_cb = 16'd35;
+		new_input_flag = ~new_input_flag;
 		//expected result: -3+39j, 7-31j
 		//a_r=1111111111111101
 		//a_c=0000000000100111
@@ -49,6 +55,7 @@ module butterfly_unit_tb;
 		i_data_ca = 16'd70;
 		i_data_rb = 16'b1111111100111000; //-200
 		i_data_cb = 16'd35;
+		new_input_flag = ~new_input_flag;
 		//expectd result: [-100.+105.j  300. +35.j]
 		//a_r=1111111110011100
 		//a_c=0000000001101001
@@ -56,6 +63,6 @@ module butterfly_unit_tb;
 		//b_c=0000000000100011
 	end
 
-	always #5 clk = ~clk;
+	always #2 clk = ~clk;
 
 endmodule
